81ac62009f105e90dca5079202259b8b4332dd54
Sairon <138011409+Yukkurisiteikitai@users.noreply.github.com>
2025-01-21 11:44:34 +0900

    setDisplay

--- src/tools/printTree.js
+++ src/tools/printTree.js
@@ -1,78 +1,8 @@
 const canvas = document.getElementById('drow');
 const ctx = canvas.getContext('2d');
 
-let commit_values = 2
-let canva_pos = [2,2]
-let pullRequest = 34
-let Disscussion = 32
-
-// ctx.fillRect(25, 25, 100, 100);
-
-750 
-
-
-
-function draw() 
-{
-    
-    if (canvas.getContext) {
-      
-  
-      for (let i = 0; i < 4; i++) {
-        for (let j = 0; j < 3; j++) {
-          ctx.beginPath();
-          const x = 25 + j * 50; // x 座標
-          const y = 25 + i * 50; // y 座標
-          const radius = 20; // 円弧の半径
-          const startAngle = 0; // 円孤の始点
-          const endAngle = Math.PI + (Math.PI * j) / 2; // 円孤の終点
-          const counterclockwise = i % 2 !== 0; // 時計回りまたは反時計回り
-  
-          ctx.arc(x, y, radius, startAngle, endAngle, counterclockwise);
-  
-          if (i > 1) {
-            ctx.fill();
-          } else {
-            ctx.stroke();
-          }
-        }
-    }
-}
-}
-
-
-function senterDrow(index){
-    ctx.beginPath();
-    ctx.lineWidth = index;
-    ctx.moveTo(750, 0);
-    ctx.lineTo(750, 876);
-    ctx.closePath();
-    ctx.stroke();
-}
-senterDrow(103);
-
-/*
-管理用のシステムはどんな仕組みにしますか?
-
-Listでの管理を考えます
-Json方式で
-
-"main":{
-
-枝の支店は木の中心なのでxは固定なので
-"start_high":24,
-"end_position":[60,86],
-"commmit":35323
-
-pullRequestは枝
-""
-
-
-}
- */
-
-
-
+//説明は src/thinks/printTreeREADME.md
+//propaty
 treeManager = {
     "main":{
         "start_high":24,
@@ -86,10 +16,70 @@ treeManager = {
     }
 }
 
+PullrequestManager = {
+    "title":{
+        "start_high":34,
+        "end_pos":[343,242],
+        "commit":23
+    }
+    
+}
+
+
+//tool
+
+function getRandomInt(min, max) {
+    const minCeiled = Math.ceil(min);
+    const maxFloored = Math.floor(max);
+    return Math.floor(Math.random() * (maxFloored - minCeiled) + minCeiled); // 上限は除き、下限は含む
+}
+
+
+//renders
+function senterDrow(index){
+    ctx.beginPath();
+    ctx.lineWidth = index;
+    ctx.moveTo(750, 0);
+    ctx.lineTo(750, 876);
+    ctx.closePath();
+    ctx.stroke();
+}
+senterDrow(103);
+
+
+function SettingMaxSizeDisplay(){
+
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientHeight;
+    console.log("LoadOK");
+    onRepaint();
+}
+
+const onRepaint = function() {
+    console.log('repaint');
+
+    const canvas =  document.getElementById('drow');
+    const context = canvas.getContext('2d');
+    
+    context.fillStyle = 'red';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    
+    context.fillStyle = 'blue';
+    context.fillRect(10, 10, canvas.width-20, canvas.height-20);
+};
+
+window.addEventListener('DOMContentLoaded', SettingMaxSizeDisplay);
+window.addEventListener('resize', SettingMaxSizeDisplay);
+
 
 
 function settingTree(high){
-    
+    ctx.beginPath();
+    ctx.lineWidth = index;
+    ctx.moveTo(750, 0);
+    ctx.lineTo(750, 876);
+    ctx.closePath();
+    ctx.stroke();
 }
 
 
@@ -97,14 +87,5 @@ function settingTree(high){
 tree 
  */
 
+//Drow Test
 
-
-
-// var drawVerticalLine = function() {
-//     ctx.beginPath();
-//     ctx.moveTo(center.x, 0);
-//     ctx.lineTo(center.x, csHeight);
-//     ctx.closePath();
-//     ctx.stroke();
-//   };
-// draw();
